76d1b8b7f3d6266f28627c8ae5f2ae94254eb0b3
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-11-27 17:17:59 +0530

    Docs : change errror message

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,6 @@
 const Error = () => (
   <div className="w-full flex justify-center items-center">
-   <h1 className='font-bold text-2x1 text-white mt-2'>Something went wrong. Please Try again.</h1>
+   <h1 className='font-bold text-2x1 text-white mt-2'>Something went wrong. Check internet connection or please try again.</h1>
   </div>
 );
 
